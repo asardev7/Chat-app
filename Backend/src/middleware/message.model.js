518d144038fb84ec8c473f8e3d51cd3551b5ea04
asardev7 <asar.dev7@gmail.com>
2026-04-11 13:34:50 +0530

    Update message.model.js

--- Backend/src/middleware/message.model.js
+++ Backend/src/middleware/message.model.js
@@ -12,12 +12,28 @@ const messageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    text: { type: String },
-    image: { type: String },
-    seen: { type: Boolean, default: false }, // ← added
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+    replyTo: {
+      messageId: { type: String, default: null },
+      text: { type: String, default: null },
+      image: { type: String, default: null },
+      senderName: { type: String, default: null },
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const Message = mongoose.model("Message", messageSchema);
+
 export default Message;
