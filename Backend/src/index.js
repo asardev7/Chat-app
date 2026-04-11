90b2a51ee8e3df7399b705529891938df65727e0
asardev7 <asar.dev7@gmail.com>
2026-04-11 10:29:53 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 import authRoutes from "./routes/authroute.js";
 import { connectDB } from "./lib/db.js";
@@ -11,20 +10,26 @@ import messageRoutes from "./routes/messageroute.js";
 import { io, server, app } from "./lib/socket.io.js";
 
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://www.ri-chat.run.place"
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get(/(.*)/, (req, res) => {
+  app.get(/(.*)/,(req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
