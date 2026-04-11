3ef4f92daa431c12955467174db3d2a81e3a1b02
asardev7 <asar.dev7@gmail.com>
2026-04-11 10:26:14 +0530

    Update message.model.js

--- Backend/src/middleware/message.model.js
+++ Backend/src/middleware/message.model.js
@@ -1,29 +1,23 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        senderid: {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-            required:true       
-        },
-        receiverid: {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-            required:true
-        },
-        text: {
-            type:String,
-        },
-        image: {
-            type:String,
-        },      
+  {
+    senderid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps:true
-    }
+    receiverid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: { type: String },
+    image: { type: String },
+    seen: { type: Boolean, default: false }, // ← added
+  },
+  { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
-
 export default Message;
