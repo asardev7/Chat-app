549a4bfa6a6c1dbd61433318ed415bf21eaf2078
asardev7 <asar.dev7@gmail.com>
2026-04-11 15:21:18 +0530

    Update user.model.js

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -28,12 +28,8 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    gifUrl: {
-  type: String,
-  default: "",
   },
-  },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
